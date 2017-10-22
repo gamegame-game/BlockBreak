91168b3f6e2423c58abe883b8f1a3a4a1f6abd1a
gamegame-game <gmgm.game1@gmail.com>
2017-10-22 23:00:27 +0900

    ゲームオーバーの判定作成

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,18 +2,21 @@
 using System.Collections.Generic;
 using UniRx;
 using UnityEngine;
+using UniRx.Triggers;
 
 public class GameManager : MonoBehaviour
 {
     public BlockProvider BlockProvider { get; private set; }
     public GameObject BlocksArea { get; private set; }
     public ScoreManager ScoreManager { get; private set; }
+    public GameObject Ground { get; private set; }
 
     void Start()
     {
         this.BlockProvider = GetComponent<BlockProvider>();
         this.BlocksArea = GameObject.Find("Blocks");
         this.ScoreManager = GetComponent<ScoreManager>();
+        this.Ground = GameObject.Find("Ground");
 
         // 配置の初期化
         InitializeStage();
@@ -54,6 +57,17 @@ public class GameManager : MonoBehaviour
         // 全ブロックが壊れたらクリア
         var stream = blocks.Select(blcok => blcok.OnBroken);
         Observable.WhenAll(stream).Subscribe(_ => GameClear());
+
+        // ゲームオーバー判定
+        this.Ground.OnTriggerEnter2DAsObservable()
+            .Subscribe(
+                collider =>
+                {
+                    // ぶつかったものを消してゲームオーバー
+                    Destroy(collider.gameObject);
+                    GameOver();
+                }
+            );
     }
 
     /// <summary>
@@ -62,5 +76,13 @@ public class GameManager : MonoBehaviour
     private void GameClear()
     {
         Debug.Log("ゲームクリア ...");
+    }
+
+    /// <summary>
+    /// ゲームオーバー処理
+    /// </summary>
+    private void GameOver()
+    {
+        Debug.Log("ゲームオーバー ...");
     }
 }
